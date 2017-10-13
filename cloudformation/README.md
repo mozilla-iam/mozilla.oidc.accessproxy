a02e0063082b5d4a179a437c1bb0883df3eab276
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-10-13 16:46:10 -0700

    Simplify CF by having a single CF regardless of environment Take parameters instead that can be defined for the env in the script or a makefile

--- cloudformation/README.md
+++ cloudformation/README.md
@@ -1,24 +1,19 @@
 # How to...
-
-Change OIDCAccessProxy by the app name - must be unique for your IAM account.
-Change the GitHub URL for your own repository as well.
-
-```
-$ sed 's/\$NAME\$/OIDCAccessProxy/g' roles.yml.tpl > roles.yml
-$ sed 's/\$NAME\$/OIDCAccessProxy/g' us-west-2.yml.tpl | sed 's/\$GIT_URL\$/https\:\/\/github\.com\/mozilla\-iam\/mozilla\.oidc\.accessproxy/' > us-west-2.yml
-```
-
 Use the same name as `$STACK_NAME` in deploy scripts and fill in other settings as needed:
 
 ```
-$ vim deploy-dev.sh
+$ vim parameters.json
 ...
-STACK_NAME=OIDCAccessProxy
+{
+    "ParameterKey": "SSHKeyName",
+    "ParameterValue": "infosec-us-west-2-keys",
+    "UsePreviousValue": false
+},
 ...
 ```
 
-Run the deploy (NOTE: it won't recreate CF templates that already exit):
+Run the deploy (NOTE: it won't recreate CF templates that already exist):
 
 ```
-$ ./deploy-dev.sh
+$ ./deploy.sh
 ```
--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -3,7 +3,6 @@
 STACK_NAME="OIDCAccessProxy"
 STACK_NAME_ROLE="${STACK_NAME}-role"
 STACK_ENV="dev"
-SSH_KEYS="infosec-us-west-2-keys"
 CREDSTASH_REGION="us-west-2"
 export AWS_DEFAULT_PROFILE="infosec-dev-admin"
 export AWS_DEFAULT_REGION="us-west-2"
@@ -17,27 +16,36 @@ function die() {
     exit $2
 }
 
+function msg() {
+    echo "$1 (Non-fatal)"
+}
+
 function create_check_wait_stack() {
     # Check if stack exists, if not, create it. Wait til all done.
     local cur_stack=$1 ; shift
     local stack_file=$1 ; shift
-    aws cloudformation describe-stack-resources --stack-name ${cur_stack} > /dev/null 2>&1 || {
-        echo Creating ${cur_stack} stack...
+    echo "Check if we need to create or update ${cur_stack}..."
+    if [[ $(aws cloudformation describe-stack-resources --stack-name ${cur_stack} > /dev/null 2>&1) ]]; then
+        echo "Creating ${cur_stack} stack..."
         aws cloudformation create-stack --stack-name ${cur_stack} --capabilities CAPABILITY_NAMED_IAM \
             --template-body ${stack_file} \
             $* \
             || die "Create stack failed" $?
-        aws cloudformation wait stack-create-complete --stack-name ${cur_stack} \
-            || die "Wait for stack complete failed" $?
-    }
-    aws cloudformation wait stack-exists --stack-name ${STACK_NAME_ROLE} || die "Wait stack exists failed" $?
+    else
+        echo "Updating ${cur_stack} stack..."
+    echo    aws cloudformation update-stack --stack-name ${cur_stack} --capabilities CAPABILITY_NAMED_IAM \
+            --template-body ${stack_file} \
+            $* \
+            || msg "Update stack failed"
+
+    fi
+    aws cloudformation wait stack-create-complete --stack-name ${cur_stack} \
+        || die "Wait for stack complete failed" $?
 }
 
 # Create stacks
 create_check_wait_stack ${STACK_NAME_ROLE} "file://roles.yml"
-create_check_wait_stack ${STACK_NAME} "file://us-west-2.yml" --parameters \
-    ParameterKey=SSHKeyName,ParameterValue=${SSH_KEYS} \
-    ParameterKey=EnvType,ParameterValue=${STACK_ENV}
+create_check_wait_stack ${STACK_NAME} "file://accessproxy.yml" --parameters file://parameters.json
 
 # Grant the IAM role permissions to decrypt
 # We do this manually so that we do not have to hard-code the credstash key id
