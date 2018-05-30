6511675b8cf2d735db5baea6da13b7d06acec808
Guillaume Destuynder (:kang) <gdestuynder@mozilla.com>
2018-05-30 13:03:03 -0700

    comma-spacing

--- etc/conf.d/server.lua
+++ etc/conf.d/server.lua
@@ -43,7 +43,7 @@ opts = {
   -- The following options are used to verify a user session should be kept running and that the user is still valid
   -- refresh_session_interval will 302 the user's browser every X amount of seconds (here, 900) transparently
   -- renew_access_token_on_expiry will use an access or refresh token with a server-side request. If you use the later
-  -- make sure you enable all 3 options: renew_access_token_on_expiry,access_token_expires_in,access_token_expires_leeway
+  -- make sure you enable all 3 options: renew_access_token_on_expiry, access_token_expires_in, access_token_expires_leeway
   -- or understand the consequences of not doing so.
   --
   --renew_access_token_on_expiry = true
